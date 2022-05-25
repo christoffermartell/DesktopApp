a30a22a5d1aeeccdf50709c7ab8102fc7b0f47c5
christoffermartell <69979256+christoffermartell@users.noreply.github.com>
2022-05-25 12:23:05 +0200

    Base added

---
+++ Core/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModernDesktopApp.Core
+{
+     class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecuted;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object>execute,Func<object, bool>canExecute = null)
+        {
+            _execute = execute;
+            _canExecuted = canExecute;
+
+        }
+
+        public bool CanExecute(object paramter)
+        {
+            return _canExecuted == null || _canExecuted(paramter);
+        }
+
+        public void Execute (object paramter)
+        {
+            _execute(paramter);
+        }
+
+
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ModernDesktopApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesktopApp.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObjects
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+            OnpropertyChanged();
+            }
+            
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
